35b939e0edbb556a3ea3ddb4e6c3680908b23bbb
GayathriGouti <gayathrigouti05@gmail.com>
2025-09-09 21:13:37 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,17 @@
+class operations:
+    #def __init__(self,a,b)
+    #self.a=a
+    #self.b=b
+    x=input("Enter the First Number: ")
+    y=input("Enter the second Number: ")
+    n1=float(x)
+    n2=float(y)
+    add=n1+n2
+    sub=n1-n2
+    mul=n1*n2
+    div= n1/n2
+    
+    print(f"Addition: {add}")
+    print(f"Subtraction: {sub}")
+    print(f"Multiplication: {mul}")
+    print(f"Division: {div}")
---
+++ welcomeprogram.py
@@ -0,0 +1,4 @@
+x=input("Enter your First name: ")
+y=input("Enter your Last name: ")
+
+print(f"Hello, {x}"+" "+f"{y}! Welcome to the python program")
