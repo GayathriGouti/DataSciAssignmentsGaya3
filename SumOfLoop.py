d8c589869a01ae311172534d350202a7bed5f0cc
GayathriGouti <gayathrigouti05@gmail.com>
2025-09-10 22:45:35 +0530

    Add files via upload

---
+++ SumOfLoop.py
@@ -0,0 +1,9 @@
+x=input("enter first number: ")
+y=input("enter second number: ")
+n1=int(x)
+n2=int(y)
+z=0
+for i in range(n1,n2+1):
+    z=z+i
+print(f"The sum of numbers from {x} to {y} is: {z}")
+    
---
+++ inbuiltmathfuns.py
@@ -0,0 +1,10 @@
+import math as m
+n=input("enter a number: ")
+x=float(n)
+a=m.sqrt(x)
+b=m.sin(x)
+c=m.log(x)
+
+print(f"squareroot is :{a}")
+print(f"sine value is:{b}")
+print(f"logarithm value is :{c}")
